be95a5f21460efbcc221a893e4074cbcaf33589b
Dominik Sznycer <biuro@alteca.com.pl>
2022-09-11 23:44:32 +0200

    added cubit&state

--- lib/main.dart
+++ lib/main.dart
@@ -38,58 +38,73 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('WITAM W CUBIT WROLD'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Text('TYLE RAZY TO BYŁO PUSZNIĘTE:'),
-          BlocBuilder<CounterCubit, CounterState>(
-            builder: (context, state) {
-              if (state.counterValue < 0) {
-                return Text(
-                  "UUUU MINUSIKKK  " + state.counterValue.toString(),
-                  style: TextStyle(fontSize: 20),
-                );
-              } else if (state.counterValue == 0) {
-                return Text(
-                  "MNIEJ NIŻ ZEROOOOO   " + state.counterValue.toString(),
-                  style: TextStyle(fontSize: 20),
-                );
-              } else {
-                return Text(
-                  "UUUU PLUSIKKK  " + state.counterValue.toString(),
-                  style: TextStyle(fontSize: 20),
-                );
-              }
-              ;
-            },
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              FloatingActionButton(
-                onPressed: () {
-                  BlocProvider.of<CounterCubit>(context).zeroShit();
-                },
-                child: Icon(Icons.exposure_zero),
-              ),
-              FloatingActionButton(
-                onPressed: () {
-                  BlocProvider.of<CounterCubit>(context).decrement();
-                },
-                tooltip: 'Decrement',
-                child: Icon(Icons.remove),
-              ),
-              FloatingActionButton(
-                onPressed: () {
-                  BlocProvider.of<CounterCubit>(context).increment();
-                },
-                tooltip: 'Increment',
-                child: Icon(Icons.add),
-              )
-            ],
-          )
-        ],
+      body: BlocListener<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if (state.isIncremented == true) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text('KURWA INCREMENT'),
+              duration: Duration(seconds: 1),
+            ));
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text('KURWA DECREMENT'),
+              duration: Duration(seconds: 1),
+            ));
+          }
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text('TYLE RAZY TO BYŁO PUSZNIĘTE:'),
+            BlocBuilder<CounterCubit, CounterState>(
+              builder: (context, state) {
+                if (state.counterValue < 0) {
+                  return Text(
+                    "UUUU MINUSIKKK  " + state.counterValue.toString(),
+                    style: TextStyle(fontSize: 20),
+                  );
+                } else if (state.counterValue == 0) {
+                  return Text(
+                    "MNIEJ NIŻ ZEROOO   " + state.counterValue.toString(),
+                    style: TextStyle(fontSize: 20),
+                  );
+                } else {
+                  return Text(
+                    "Jesteś na plus!   " + state.counterValue.toString(),
+                    style: TextStyle(fontSize: 20),
+                  );
+                }
+                ;
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).zeroShit();
+                  },
+                  child: Icon(Icons.exposure_zero),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  tooltip: 'Decrement',
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
